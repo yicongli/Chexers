2c8a3544cb3f20cdc0eed735233fa5c910b8c470
Yicong Li <liyicong.china@gmail.com>
2019-04-21 16:06:07 +1000

    Init project

---
+++ Chexers/ChexersTests/ChexersTests.swift
@@ -0,0 +1,34 @@
+//
+//  ChexersTests.swift
+//  ChexersTests
+//
+//  Created by Yicong Li on 21/4/19.
+//  Copyright © 2019 Yicong Li. All rights reserved.
+//
+
+import XCTest
+@testable import Chexers
+
+class ChexersTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
